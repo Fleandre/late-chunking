a760d89cba187844da03383afd7d8266607f5fb2
zhouyunhao <zhouyunhao.zyh@antgroup.com>
2024-10-12 15:39:26 +0800

    run eval in subprocess which tries to find the best batch_size

--- run_chunking_benchmark.py
+++ run_chunking_benchmark.py
@@ -1,9 +1,9 @@
-import click
 import json
 import torch.cuda
 from mteb import MTEB
 from transformers import AutoModel, AutoTokenizer
 import itertools
+import multiprocessing
 
 from chunked_pooling.chunked_eval_tasks import *
 from chunked_pooling.wrappers import load_model
@@ -70,8 +70,78 @@ def get_valid_setting_str(eval_setting, exist_results, overwrite=False):
     return setting_key
 
 
+def run_eval(eval_setting_str, task_name_to_cls, batch_size, benchmark, return_dict):
+    eval_setting = json.loads(eval_setting_str)
+    print(f"Evaluation Setting: {eval_setting}")
+    task_cls = task_name_to_cls[eval_setting["task"]]
+    model_name = eval_setting["model_name"]
+    strategy = eval_setting["strategy"]
+    chunk_size = eval_setting["chunk_size"]
+    n_sentences = 5
+    chunking_model = None
+    truncate_max_length = None
+
+    # build dependencies
+    model, has_instructions = load_model(model_name)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+
+    if torch.cuda.is_available():
+        model = model.cuda()
+    model.eval()
+
+    chunking_args = {
+        "chunk_size": chunk_size,
+        "n_sentences": n_sentences,
+        "chunking_strategy": strategy,
+        "model_has_instructions": has_instructions,
+        "embedding_model_name": (chunking_model if chunking_model else model_name),
+    }
+
+    tasks = [
+        task_cls(
+            tokenizer=tokenizer,
+            prune_size=None,
+            truncate_max_length=truncate_max_length,
+            **chunking_args,
+        )
+    ]
+
+    # Evaluate
+    evaluation = MTEB(
+        tasks=tasks,
+        tokenizer=tokenizer,
+        prune_size=None,
+        **chunking_args,
+    )
+    try:
+        res = evaluation.run(
+            model,
+            output_folder="results",
+            eval_splits=tasks[0].eval_splits,
+            overwrite_results=True,
+            encode_kwargs={"batch_size": batch_size},
+            verbosity=0,
+        )
+    except:
+        print(f"Batch size {batch_size} is too large. Reduce batch size and try again.")
+        return_dict[eval_setting_str] = (-1, None)
+        return return_dict
+
+    # 保存结果
+    res_dict = res[0].to_dict()
+    res_dict["model_name"] = model_name
+    res_dict["chunking_strategy"] = strategy
+    res_dict["chunk_size"] = chunk_size
+    res_dict["n_sentences"] = n_sentences
+    res_dict["chunking_model"] = chunking_model
+
+    return_dict[eval_setting_str] = (batch_size, res_dict)
+
+    return return_dict
+
+
 def main():
-    benchmark = []
     task_name_to_cls = get_eval_tasks()
 
     # 排除部分不测试的数据集
@@ -104,84 +174,48 @@ def main():
     # 笛卡尔集
     param_names = ["task", "strategy", "chunk_size", "model_name"]
     combinations = itertools.product(task_names, strategies, chunk_size_list, models)
-    eval_settings = [dict(zip(param_names, combo)) for combo in combinations]
-    # 去除非法和重复的组合
-    evaluated_key, existing_result = load_existing_results()
-    benchmark.extend(existing_result)
-    eval_settings = set(
-        get_valid_setting_str(eval_setting, evaluated_key, OVERWRITE)
-        for eval_setting in eval_settings
-    )
-    # json转换为dict
-    eval_settings = [
-        json.loads(eval_setting)
-        for eval_setting in eval_settings
-        if eval_setting is not None
-    ]
+    combinations = [dict(zip(param_names, combo)) for combo in combinations]
+    # 去除非法和已测试的组合
+    evaluated_key, benchmark = load_existing_results()
+    eval_settings = set()
+    for combo in combinations:
+        valid_setting = get_valid_setting_str(combo, evaluated_key, OVERWRITE)
+        if valid_setting is not None:
+            eval_settings.add(valid_setting)
 
-    for eval_setting in eval_settings:
-        print(f"Evaluation Setting: {eval_setting}")
-        task_cls = task_name_to_cls[eval_setting["task"]]
-        model_name = eval_setting["model_name"]
-        strategy = eval_setting["strategy"]
-        chunk_size = eval_setting["chunk_size"]
-        n_sentences = 5
-        chunking_model = None
-        truncate_max_length = None
-
-        # build dependencies
-        model, has_instructions = load_model(model_name)
-
-        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-
-        if torch.cuda.is_available():
-            model = model.cuda()
-        model.eval()
-
-        chunking_args = {
-            "chunk_size": chunk_size,
-            "n_sentences": n_sentences,
-            "chunking_strategy": strategy,
-            "model_has_instructions": has_instructions,
-            "embedding_model_name": (chunking_model if chunking_model else model_name),
-        }
-
-        tasks = [
-            task_cls(
-                tokenizer=tokenizer,
-                prune_size=None,
-                truncate_max_length=truncate_max_length,
-                **chunking_args,
+    # 定义尝试的batch size列表，从大到小
+    batch_sizes = [32, 16, 8, 4, 2, 1]
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    for i, eval_setting in enumerate(eval_settings):
+        for batch_size in batch_sizes:
+            p = multiprocessing.Process(
+                target=run_eval,
+                args=(
+                    eval_setting,
+                    task_name_to_cls,
+                    batch_size,
+                    benchmark,
+                    return_dict,
+                ),
             )
-        ]
+            p.start()
+            p.join()
 
-        # Evaluate
-        evaluation = MTEB(
-            tasks=tasks,
-            tokenizer=tokenizer,
-            prune_size=None,
-            **chunking_args,
-        )
-        res = evaluation.run(
-            model,
-            output_folder="results",
-            eval_splits=tasks[0].eval_splits,
-            overwrite_results=True,
-            encode_kwargs={"batch_size": BATCH_SIZE},
-            verbosity=0,
-        )
+            # 检查是否成功运行
+            optimal_batch_size, res_dict = return_dict[eval_setting]
+            if optimal_batch_size != -1:
+                print(
+                    f"Parameters: {eval_setting}, Optimal batch size: {optimal_batch_size}"
+                )
+                # 保存结果
+                benchmark.append(res_dict)
+                with open(f"{OUTPUT_DIR}/benchmark.json", "w", encoding="utf-8") as f:
+                    json.dump(benchmark, f, ensure_ascii=False, indent=4)
+                break
 
-        # 保存结果
-        res_dict = res[0].to_dict()
-        res_dict["model_name"] = model_name
-        res_dict["chunking_strategy"] = strategy
-        res_dict["chunk_size"] = chunk_size
-        res_dict["n_sentences"] = n_sentences
-        res_dict["chunking_model"] = chunking_model
-
-        benchmark.append(res_dict)
-        with open(f"{OUTPUT_DIR}/benchmark.json", "w", encoding="utf-8") as f:
-            json.dump(benchmark, f, ensure_ascii=False, indent=4)
+    with open(f"{OUTPUT_DIR}/batch_size.json", "w", encoding="utf-8") as f:
+        json.dump(dict(return_dict), f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
