32a2b9cd443eceef020226006efdf6a0daa2977b
zhouyunhao <zhouyunhao.zyh@antgroup.com>
2024-10-16 11:11:33 +0800

    fix semantic chunking modol loading problem

--- chunked_pooling/chunking.py
+++ chunked_pooling/chunking.py
@@ -54,6 +54,7 @@ class Chunker:
 
         self.langchain_embed_model = langchian_hf_embedding(
             model_name=self.embedding_model_name,
+            model_kwargs={"trust_remote_code": True},
         )
         self.langchain_splitter = SemanticChunker(
             self.langchain_embed_model,
